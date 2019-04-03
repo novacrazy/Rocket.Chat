c152daa546e78c12c85200a821b5c9ef07ade8d1
novacrazy <novacrazy@gmail.com>
2019-04-02 19:40:58 -0500

    Improve handling of large embed titles

--- packages/rocketchat-oembed/client/oembedUrlWidget.js
+++ packages/rocketchat-oembed/client/oembedUrlWidget.js
@@ -37,8 +37,10 @@ Template.oembedUrlWidget.helpers({
 			if(title.length >= MAX_LENGTH) {
 				let last_comma = title.lastIndexOf(',', MAX_LENGTH);
 
-				if(last_comma > 0) {
+				if(last_comma > 0 && (MAX_LENGTH - last_comma) < 50) {
 					return Blaze._escape(title.slice(0, last_comma) + ', …');
+				} else {
+					return Blaze._escape(title.slice(0, MAX_LENGTH) + '…');
 				}
 			}
 			return Blaze._escape(title);
