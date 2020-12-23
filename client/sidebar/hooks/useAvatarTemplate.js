96e3f018b61656bb114d4f70ee6b16d378f8b4c8
novacrazy <novacrazy@gmail.com>
2020-12-23 08:42:25 -0600

    Disable avatar rounding

--- client/sidebar/hooks/useAvatarTemplate.js
+++ client/sidebar/hooks/useAvatarTemplate.js
@@ -23,6 +23,6 @@ export const useAvatarTemplate = () => {
 			}
 		})();
 
-		return (room) => <RoomAvatar size={size} rounded room={{ ...room, _id: room.rid || room._id, type: room.t }} />;
+		return (room) => <RoomAvatar size={size} room={{ ...room, _id: room.rid || room._id, type: room.t }} />;
 	}, [sidebarHideAvatar, sidebarViewMode]);
 };
